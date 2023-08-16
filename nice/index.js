fae052998d32f5fa54b3481eace926e99ee3c27c
Igorsml <igorsmlff@gmail.com>
2023-08-16 07:39:21 +0300

    feat: mini long polling

---
+++ nice/index.js
@@ -0,0 +1,47 @@
+const http = require("http");
+
+const clients = [];
+const bets = [];
+
+const server = http.createServer((request, response) => {
+  switch (request.url) {
+    case "/":
+      mainPage(request, response);
+      break;
+
+    case "/bet":
+      betPage(request, response);
+      break;
+
+    default:
+      response.writeHead(404);
+      response.end("Page not found");
+  }
+});
+
+server.listen(3000);
+
+function mainPage(request, response) {
+  clients.push({ request, response });
+  response.writeHead(200);
+
+  response.on("end", () => cleanClient(response));
+}
+
+function betPage(request, response) {
+  bets.push(Math.random());
+  sendBetsForAll();
+  response.writeHead(200);
+
+  response.end("done");
+}
+
+function sendBetsForAll() {
+  clients.forEach((client) => client.response.end(JSON.stringify(bets)));
+}
+
+function cleanClient(response) {
+  let index = clients.findIndex((client) => client.response === response);
+
+  if (index !== -1) clients.splice(index, 1);
+}
